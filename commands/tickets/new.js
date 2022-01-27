2e6a645f2e55a504cadef73d071ce91ed91ffd48
Muneeb <i@muneeb.eu.org>
2022-01-28 02:36:15 +0530

    Add files via upload

---
+++ commands/tickets/new.js
@@ -0,0 +1,17 @@
+const Utils = require("../../modules/utils");
+const config = Utils.variables.config;
+const lang = Utils.variables.lang;
+const createTicket = require('../../modules/methods/createTicket');
+
+module.exports = {
+    name: 'new',
+    run: async (bot, message, args) => {
+        createTicket(bot, args, message.member, message.channel);
+    },
+    description: "Create a ticket",
+    usage: config.Tickets.RequireReason ? 'new <reason>' : 'new [reason]',
+    aliases: [
+        'ticket'
+    ]
+}
+// 239232   8501   2161065    52657   1611445495   2f07bbd86d5ed3caf75117a749dec23e7538b2a3   2161065
