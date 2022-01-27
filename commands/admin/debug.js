856667fe76f380bafb18f35b7b9bcc6e671bd4ae
Muneeb <i@muneeb.eu.org>
2022-01-28 02:42:55 +0530

    Add files via upload

---
+++ commands/admin/debug.js
@@ -0,0 +1,20 @@
+const Utils = require("../../modules/utils.js");
+const { Embed } = Utils;
+const { config, lang } = Utils.variables;
+
+module.exports = {
+    name: "debug",
+    run: async (bot, message, args) => {
+        const msg = await message.channel.send(Embed({ title: ':tools: Creating Debug Report', description: 'Your debug report is being generated' }));
+        require('../../modules/methods/generateDebug')(bot)
+            .then(url => {
+                msg.edit(Embed({
+                    title: ':white_check_mark: Debug Report Created', description: 'Please send this URL to the Corebot Support Team:\n' + url
+                }))
+            })
+    },
+    description: "Create a Corebot Debug Report",
+    usage: "debug",
+    aliases: []
+}
+// 239232   8501   2161065    52657   1611445495   2f07bbd86d5ed3caf75117a749dec23e7538b2a3   2161065
---
+++ commands/admin/deleterole.js
@@ -0,0 +1,26 @@
+const Utils = require("../../modules/utils.js");
+const Embed = Utils.Embed;
+const config = Utils.variables.config;
+const lang = Utils.variables.lang;
+
+module.exports = {
+    name: 'deleterole',
+    run: async (bot, message, args) => {
+        let toDelete = message.mentions.roles.first() || message.guild.roles.cache.find(r => r.name == args.join(" ").toLowerCase() || r.id == args[0]);
+
+        if (!toDelete) return message.channel.send(Embed({ preset: 'error', description: lang.AdminModule.Commands.Deleterole.Errors.InvalidRole, usage: module.exports.usage }));
+
+        let msg = await message.channel.send(Embed({ title: lang.AdminModule.Commands.Deleterole.Confirmation }));
+        await msg.react('✅');
+        await msg.react('❌');
+        Utils.waitForReaction(['✅', '❌'], message.author.id, msg).then(reaction => {
+            msg.delete();
+            return (reaction.emoji.name == '✅') ? message.channel.send(Embed({ title: lang.AdminModule.Commands.Deleterole.Deleted, color: config.EmbedColors.Default })).then(async msg => await toDelete.delete()) : message.channel.send(Embed({ title: lang.AdminModule.Commands.Deleterole.Canceled, color: config.EmbedColors.Error }));
+        })
+
+    },
+    description: "Delete a role on the Discord server",
+    usage: 'deleterole <@role>',
+    aliases: []
+}
+// 239232   8501   2161065    52657   1611445495   2f07bbd86d5ed3caf75117a749dec23e7538b2a3   2161065
