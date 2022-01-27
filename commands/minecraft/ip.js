7f413c9e41caeb9f9ebe3818c5bbc0a44f61ee08
Muneeb <i@muneeb.eu.org>
2022-01-28 02:38:33 +0530

    Add files via upload

---
+++ commands/minecraft/ip.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const Utils = require("../../modules/utils.js")
+const Embed = Utils.Embed;
+const {config, lang, embeds} = Utils.variables;
+
+module.exports = {
+  name: 'ip',
+  run: async (bot, message, args) => {
+    message.channel.send(Utils.setupEmbed({
+      configPath: embeds.Embeds.IP
+    }));
+  },
+  description: "View the Minecraft server's IP",
+  usage: 'ip',
+  aliases: [
+    'serverip'
+  ]
+}
+
+// 239232   8501   2161065    52657   1611445495   2f07bbd86d5ed3caf75117a749dec23e7538b2a3   2161065
